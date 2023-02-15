b2e8e11b007d2f54e6fdede456794237738f4924
Peter Ng <peter.nguyentr@gmail.com>
2023-02-15 09:49:48 +0100

    fix: ReferenceError: ansiRegExp is not defined

--- index.js
+++ index.js
@@ -313,7 +313,7 @@ module.exports = (config) => {
 };
 
 
-module.exports.ansiRegExp = function ({ onlyFirst = false } = {}) {
+const ansiRegExp = function ({ onlyFirst = false } = {}) {
   const pattern = [
     '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
     '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))',
